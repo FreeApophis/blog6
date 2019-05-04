a8dd2657db1108286ccf8a9aee677b7d432764e4
Thomas Bruderer <apophis@apophis.ch>
2019-05-04 11:23:06 +0200

    Add Comment Admin Interface

---
+++ app/admin/comments_admin.rb
@@ -0,0 +1,19 @@
+Trestle.resource(:comments) do
+  menu do
+    item :comments, icon: "fa fa-comments", group: :content
+  end
+
+  table do
+    column :text
+    column :created_at, align: :center
+    actions
+  end
+
+  form do |comment|
+    text_area :text
+  end
+
+  params do |params|
+    params.require(:comment).permit(:text)
+  end
+end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CreateCommentTest < ActiveSupport::TestCase
+class CommentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end

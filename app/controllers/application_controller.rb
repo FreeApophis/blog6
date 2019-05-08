59834ae4dbacef7df2c89c2f2e2e0e4253b08450
Thomas Bruderer <apophis@apophis.ch>
2019-05-08 19:31:12 +0200

    Switch layouts dynamically

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,17 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  layout :get_layout
+
   def current_tenant
     domain = Domain.find_by url: request.host
     if domain && domain.tenant
       domain.tenant
     end
+  end
+
+  def get_layout
+    'application'
   end
 
   helper_method :current_tenant

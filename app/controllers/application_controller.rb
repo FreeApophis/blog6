4ff7ff586d3334837ca6860a965c651c524451c0
Thomas Bruderer <apophis@apophis.ch>
2019-05-10 19:47:43 +0200

    Update design and make tardis available as render block

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery
 
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
 
   layout :get_layout
 

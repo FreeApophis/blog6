dd8cd4dfefa1047c712362f5e2e710c124f5079a
Thomas Bruderer <apophis@apophis.ch>
2019-08-19 14:46:55 +0200

    Trix sucks so much...

--- app/javascript/packs/trix.js
+++ app/javascript/packs/trix.js
@@ -7,7 +7,18 @@ require("@rails/activestorage").start()
 
 const trix = require("trix")
 
+// trix default behaviour is terrible:
+// convention over configuration only works if the conventions are sane
+
+// uses h2 instead of h1 (h1 is the site title)
 trix.config.blockAttributes.heading1.tagName = "h2"
+
+// wtf, use the paraghraph tag
 trix.config.blockAttributes.default.tagName = "p"
+
+
+// by default, trix can't handle relative links...
+input = trix.config.toolbar.content.querySelector("input[type=url]")
+input.type = "text"
 
 require("@rails/actiontext")

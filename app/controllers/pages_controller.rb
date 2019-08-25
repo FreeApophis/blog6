4e99a7c7ab55946fb57504cf9a9af4cfb67afa87
Thomas Bruderer <apophis@apophis.ch>
2019-08-25 14:33:17 +0200

    Inhibit access to pages on other tenants

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,13 @@ class PagesController < ApplicationController
   before_action :set_page, only: [:show]
 
   def index
-    @pages = Page.all
+    @pages = secure_scope
   end
 
   def show
+    if !@page.tenants.include?(current_tenant)
+      redirect_to :root, notice: @page.tenants.include?(current_tenant)
+    end
   end
 
   private
